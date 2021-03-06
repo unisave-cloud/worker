07b5243a03afb07e6d3b5def48db9b2d66c1e240
Jirka-Mayer <jirkamayer97@gmail.com>
2021-03-06 23:54:03 +0100

    Added simple function deployment script

---
+++ UnisaveSandbox/deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# simple function deployment script to test things out
+
+REGISTRY_AUTH=$(kubectl get secret/do-registry-key -o jsonpath={.data.\\.dockerconfigjson})
+
+curl -X POST --user root:password http://openfaas.unisave.local/system/functions -d '{
+	"service": "sandbox",
+	"image": "registry.digitalocean.com/unisave/unisave-sandbox:0.5.0-dev",
+	"labels": {},
+	"annotations": {},
+	"envVars": {
+		"INITIALIZATION_RECIPE_URL": "http://web.unisave-dev.svc.cluster.local:8000/_api/sandbox-recipe/123/pEKzqMtILv0CirCCYytGDoZyLCgYvLwi/39ea2c46ca1cbd03a463a9cd586c39d41903f45a4b608ef406fdf57254ea6914",
+		"fprocess": "ls -l"
+	},
+	"secrets": ["do-registry-key"],
+	"limits": {
+		"memory": "128M",
+		"cpu": "0.01"
+    },
+    "registryAuth": "'$REGISTRY_AUTH'"
+}'
