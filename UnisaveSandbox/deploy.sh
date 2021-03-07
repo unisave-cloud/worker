cf598a3ddaa45abc1ea203e22a33989d6ca34e56
Jirka-Mayer <jirkamayer97@gmail.com>
2021-03-07 01:23:17 +0100

    Solved deployment issues

--- UnisaveSandbox/deploy.sh
+++ UnisaveSandbox/deploy.sh
@@ -10,13 +10,11 @@ curl -X POST --user root:password http://openfaas.unisave.local/system/functions
 	"labels": {},
 	"annotations": {},
 	"envVars": {
-		"INITIALIZATION_RECIPE_URL": "http://web.unisave-dev.svc.cluster.local:8000/_api/sandbox-recipe/123/pEKzqMtILv0CirCCYytGDoZyLCgYvLwi/39ea2c46ca1cbd03a463a9cd586c39d41903f45a4b608ef406fdf57254ea6914",
-		"fprocess": "ls -l"
+		"INITIALIZATION_RECIPE_URL": "http://web.unisave-dev.svc.cluster.local:8000/_api/sandbox-recipe/123/pEKzqMtILv0CirCCYytGDoZyLCgYvLwi/39ea2c46ca1cbd03a463a9cd586c39d41903f45a4b608ef406fdf57254ea6914"
 	},
 	"secrets": ["do-registry-key"],
 	"limits": {
-		"memory": "128M",
-		"cpu": "0.01"
+		"memory": "70M"
     },
     "registryAuth": "'$REGISTRY_AUTH'"
 }'
--- UnisaveSandbox/initialization.sh
+++ UnisaveSandbox/initialization.sh
@@ -3,5 +3,6 @@
 # This script is the entrypoint of the function container.
 # It performs initialization and then starts the watchdog.
 
-mono --debug /sandbox/UnisaveSandbox.exe init $INITIALIZATION_RECIPE_URL \
-    && fwatchdog
+mono --debug /sandbox/UnisaveSandbox.exe init $INITIALIZATION_RECIPE_URL
+
+fwatchdog
